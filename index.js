54a6f3a14816bc7abe29bb16a6554577a4ee9e1e
Shruthi Kumar <sk593@duke.edu>
2020-11-13 12:23:20 -0500

    testing movie pages

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ app.get("/api/getmovies", (req, res) => {
 
 app.get("/api/getmovie", (req, res) => {
   let movieid = req.query.id;
+  console.log(movieid);
   const sqlSelect = "SELECT * FROM Movies where movieid = ?;";
   db.query(sqlSelect, [movieid], (err, result) => {
       res.json(result);
