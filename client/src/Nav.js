0923877ee8a23c6aea5dd00e4955e0c58c8824aa
sas182 <sas182@duke.edu>
2020-11-16 22:37:14 -0500

    css

--- client/src/Nav.js
+++ client/src/Nav.js
@@ -9,7 +9,7 @@ function Nav() {
         color: 'white',
         fontWeight: 'bold',
         textDecoration: 'none',
-        background: 'gray',
+        background: '#857fda',
         boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
         padding: '10px',
         borderRadius: '5px'
