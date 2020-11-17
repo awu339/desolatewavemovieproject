2fb245400aeccc05124d8a10d815cd33722604fd
sas182 <sas182@duke.edu>
2020-11-16 22:41:08 -0500

    css

--- client/src/Nav.js
+++ client/src/Nav.js
@@ -6,10 +6,10 @@ import logo from './cinema.png';
 function Nav() {
     const type = localStorage.getItem('type');
     const navStyle = {
-        color: 'white',
+        color: 'black',
         fontWeight: 'bold',
         textDecoration: 'none',
-        background: '#857fda',
+        background: 'whitesmoke',
         boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
         padding: '10px',
         borderRadius: '5px'
